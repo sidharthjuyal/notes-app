8699578baeed9d8d2401a4575952fd2cb8737dcd
Sidharth juyal <sid.juyal.34@gmail.com>
2022-09-04 15:28:46 +0530

    update v1.5

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ let content = document.getElementById("content");
 let backer = document.getElementById("backer");
 let picture = document.getElementById("img");
 var butCount = false;
+let deleteFlag = 0;
 
 function createReminder(id) {
-  if (note.value == "" || picture.value =="") {
+  if (note.value == "" || picture.value == "") {
     alert("Some fields are empty!");
   }
   else {
@@ -25,7 +26,7 @@ function createReminder(id) {
     image.src = pic;
     imgDiv.appendChild(image);
     div.appendChild(imgDiv);
-     
+
 
     let para = document.createElement('p');
     para.innerText = text;
@@ -33,7 +34,7 @@ function createReminder(id) {
     para.style.marginRight = "1rem";
     para.style.marginLeft = "1rem";
     div.appendChild(para);
-    
+
 
     let button = document.createElement("button");
     button.innerText = "Delete";
@@ -45,6 +46,7 @@ function createReminder(id) {
       if (div.id == id) {
         content.removeChild(div);
         localStorage.removeItem(id);
+        deleteFlag++;
       }
     });
 
@@ -58,14 +60,13 @@ function createReminder(id) {
         let modify = prompt("Enter your memory: ");
         para.innerText = modify;
         localStorage.removeItem(id);
-        localStorage.setItem(id,div.innerHTML);
+        localStorage.setItem(id, div.innerHTML);
       }
     });
 
     return div;
   }
 }
-
 
 but.addEventListener("click", function () {
 
@@ -85,55 +86,54 @@ let serbut = document.getElementById("serbut");
 var searcher = 0;
 
 serbut.addEventListener('click', function () {
-  if(searchy.value=="")
-  alert("Search can't be empty!");
-  else
-  {
-  let divs = document.querySelectorAll(".cont");
-  Array.from(divs).forEach(function (element) {
-    let str = element.firstElementChild.nextElementSibling.innerText;
-    // console.log(str);
-    if (str.search(searchy.value) == -1)
-      element.style.display = "none";
-    else
-      ++searcher;
-  });
-  if (searcher == 0) {
-    var para = document.createElement('p');
-    para.innerHTML = "No element found!";
-    para.className = "bold";
-    para.style.marginRight = "1rem";
-    if (backer.firstElementChild == undefined)
-      backer.appendChild(para);
-  }
-  else
-    searcher = 0;
-
-  var back = document.createElement('button');
-  back.innerText = "Back";
-  back.className = "btn";
-  back.style.alignSelf = "center";
-  if (butCount == false) {
-    backer.appendChild(back);
-    butCount = true;
-  }
-
-  back.addEventListener('click', function () {
+  if (searchy.value == "")
+    alert("Search can't be empty!");
+  else {
+    let divs = document.querySelectorAll(".cont");
     Array.from(divs).forEach(function (element) {
-      element.style.display = "block";
+      let str = element.firstElementChild.nextElementSibling.innerText;
+      // console.log(str);
+      if (str.search(searchy.value) == -1)
+        element.style.display = "none";
+      else
+        ++searcher;
     });
-    backer.removeChild(back);
-    butCount = false;
-    if (para != undefined)
-      backer.removeChild(para);
-  });
-}
+    if (searcher == 0) {
+      var para = document.createElement('p');
+      para.innerHTML = "No element found!";
+      para.className = "bold";
+      para.style.marginRight = "1rem";
+      if (backer.firstElementChild == undefined)
+        backer.appendChild(para);
+    }
+    else
+      searcher = 0;
+
+    var back = document.createElement('button');
+    back.innerText = "Back";
+    back.className = "btn";
+    back.style.alignSelf = "center";
+    if (butCount == false) {
+      backer.appendChild(back);
+      butCount = true;
+    }
+
+    back.addEventListener('click', function () {
+      Array.from(divs).forEach(function (element) {
+        element.style.display = "block";
+      });
+      backer.removeChild(back);
+      butCount = false;
+      if (para != undefined)
+        backer.removeChild(para);
+    });
+  }
 });
 
 
 // localStorage if closing web browser or tab change or reload
-if(content.firstElementChild == null) {
-  for(let i=0; i<localStorage.length; i++){
+if (content.firstElementChild == null) {
+  for (let i = 0; i < localStorage.length; i++) {
     let key = localStorage.key(i);
     let value = localStorage.getItem(key);
     let div = document.createElement('div');
@@ -148,35 +148,23 @@ if(content.firstElementChild == null) {
       if (div.id == key) {
         content.removeChild(div);
         localStorage.removeItem(key);
+        deleteFlag++;
       }
     });
 
-      let button2 = div.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling;
+    let button2 = div.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling;
     // modify button
     button2.addEventListener('click', function () {
       if (div.id == key) {
         let modifier = prompt("Enter your memory: ");
         div.firstElementChild.nextElementSibling.innerText = modifier;
         localStorage.removeItem(key);
-        localStorage.setItem(key,div.innerHTML);
+        localStorage.setItem(key, div.innerHTML);
       }
     });
   }
 }
 
-// footer
-
-var body = document.body,
-    html = document.documentElement;
-
-var height = Math.max( body.scrollHeight, body.offsetHeight, 
-                       html.clientHeight, html.scrollHeight, html.offsetHeight );
-
-if(height > 1000){
-  document.getElementById("foot").style.backgroundColor = "forestgreen";
-  document.getElementById("foot").style.color = "white";
-}
-  
 
 
 
